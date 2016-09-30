e55d811434850fec26d802138b34163d20b80923
Ivan Montiel <idmontie@asu.edu>
2016-09-30 09:05:19 -0700

    Add react a11y

--- MeteorApp/client/main.js
+++ MeteorApp/client/main.js
@@ -4,6 +4,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
 
+import a11y from 'react-a11y';
 import taskReducer from '/imports/modules/tasks/reducers';
 import App from '/imports/modules/app/components/App';
 
@@ -14,6 +15,10 @@ const store = createStore(combineReducers(
 ));
 
 Meteor.startup(() => {
+  if (Meteor.isDevelopment) {
+    a11y(React);
+  }
+
   render(
     <Provider store={store}>
       <App />
