40f234f5bce27e3b460068aa2e0c8a6633f2c424
Ivan Montiel <idmontie@asu.edu>
2016-10-01 21:56:18 -0700

    Get tests to work from the command line

---
+++ MeteorApp/imports/modules/tasks/collections/tasks.tests.js
@@ -0,0 +1,44 @@
+import { Meteor } from 'meteor/meteor';
+import { expect } from 'chai';
+
+import { Tasks } from './tasks.js';
+
+const { describe, it, beforeEach } = global;
+
+if (Meteor.isServer) {
+  describe('Tasks', () => {
+    describe('methods', () => {
+      let createdId;
+
+      beforeEach(() => {
+        Tasks.remove({});
+        createdId = Tasks.insert({
+          text: 'Test',
+          createdAt: new Date(),
+        });
+      });
+
+      it('creates a task', () => {
+        const task = 'test task';
+
+        const createTask = Meteor.server.method_handlers['tasks.insert'];
+
+        const invocation = {};
+
+        createTask.apply(invocation, [task]);
+
+        expect(Tasks.find().count()).to.be.equal(2);
+      });
+
+      it('deletes a task', () => {
+        const deleteTask = Meteor.server.method_handlers['tasks.remove'];
+
+        const invocation = {};
+
+        deleteTask.apply(invocation, [createdId]);
+
+        expect(Tasks.find().count()).to.be.equal(0);
+      });
+    });
+  });
+}
